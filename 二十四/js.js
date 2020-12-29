6b28bc82396c9b9a526ef3360a4d408fd2e32e79
hjj18279003807 <18279003807@139.com>
2020-12-29 10:26:50 +0800

    24次课

---
+++ 二十四/js.js
@@ -0,0 +1,16 @@
+var pitaya3 = {}
+console.log(typeof pitaya3)
+
+var pitaya4 = new Object();
+
+
+console.log(pitaya3 instanceof Object)
+console.log(pitaya4 instanceof Object)
+
+const pitaya5 = 100;
+pitaya5 = 200
+
+const pitaya6 = {name:"zhangsan"
+}
+
+pitaya6.name = "lisi"
