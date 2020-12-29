18589b4acc190d6a7b012812b24414ab8378abb1
hjj18279003807 <18279003807@139.com>
2020-12-29 10:32:33 +0800

    28次课

---
+++ 二十八次课/vue/vue01/src/components/my.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wapper">
+    <div id="name">江西软件大学</div>
+    <div id="content">江西软件大学简历</div>
+    <div v-for="index in lists" :key="index">
+      {{ index.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lists: [{ name: "张三" }, { name: "李四" }],
+      information: null
+    }
+  }
+}
+</script>
+<style  scoped lang="less" rel="stylesheet/less">
+#wapper {
+  background-color: red;
+  #name {
+    background-color: orange;
+  }
+}
+</style>
